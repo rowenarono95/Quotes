d76583ef0d59403f33fb7213d683ad6be25f8090
rowenarono95 <rowenarono@gmail.com>
2020-09-28 00:26:21 +0300

    add the downvote functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
-import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     FormComponent,
-    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,8 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(0, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
     new Quote(1, "Life is not measured by the number of breaths you take but by the moments that take your breath away.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
-    new Quote(2, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
-    new Quote(3, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0)
+    new Quote(2, "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.", "Martin Luther King Jr.", "Rowena Rono", new Date(), 0, 0),
+    new Quote(3, "Shallow understanding from people of good will is more frustrating than absolute misunderstanding from people of ill will.", "Martin Luther King Jr.", "Rowena Rono", new Date(), 0, 0)
   ]
   upVotes = 0;
   downVotes = 0;
