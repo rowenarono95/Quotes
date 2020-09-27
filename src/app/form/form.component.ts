a32d627b2a841aa1cb80896626e1c9c6d46e4ef3
rowenarono95 <rowenarono@gmail.com>
2020-09-27 23:34:23 +0300

    add votes functionality

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -8,6 +8,13 @@ import { Quote } from '../quote';
 })
 export class FormComponent implements OnInit {
   newQuote = new Quote(0,"","","",new Date(),0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>()
+  submitQuote(){
+    
+    this.addQuote.emit(this.newQuote);
+    
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote'
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -7,13 +7,24 @@ import {Quote} from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes: Quote[]=[
-    new Quote(0,"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.","Maya Angelou","Rowena Rono",new Date(),0,0),
-    new Quote(1,"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.","Maya Angelou","Rowena Rono",new Date(),0,0),
-    new Quote(2,"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.","Maya Angelou","Rowena Rono",new Date(),0,0),
-    new Quote(3,"I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.","Maya Angelou","Rowena Rono",new Date(),0,0)
+  quotes: Quote[] = [
+    new Quote(0, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
+    new Quote(1, "Life is not measured by the number of breaths you take but by the moments that take your breath away.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
+    new Quote(2, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0),
+    new Quote(3, "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", "Maya Angelou", "Rowena Rono", new Date(), 0, 0)
   ]
+  upVotes = 0;
+  downVotes = 0;
 
+
+  newQuote(quote) {
+    quote.author = quote.author
+    quote.quote = quote.quote
+    quote.user = quote.user
+    this.quotes.push(quote)
+
+
+  }
   constructor() { }
 
   ngOnInit(): void {
